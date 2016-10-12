d042a56e4702e0557c8c02cb7db1c68fa7c70f84
yuqing.xia <xiayuqing123@hotmail.com>
2016-10-12 17:49:23 +0800

    update build.cmd

--- build.cmd
+++ build.cmd
@@ -10,10 +10,10 @@ set DIST_BIN_DIR=lts-%VERSION%-bin
 
 md "%BASE_HOME%\dist"
 
-set STARTUP_DIR=%BASE_HOME%\lts-startup
+set STARTUP_DIR=%BASE_HOME%\auto-man-startup
 cd %STARTUP_DIR%
 start mvn clean assembly:assembly -DskipTests
 echo "Auto-Man: mvn clean assembly:assembly -DskipTests"
 echo "Auto-Man: After sub window finished, close it , and press any key to continue" & pause>nul
-
+del "%STARTUP_DIR%\target\auto-man-%VERSION%-bin.zip"
 xcopy /e /y "%STARTUP_DIR%\target\auto-man-%VERSION%-bin.zip" "%BASE_HOME%\dist"
